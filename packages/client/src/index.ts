c93dd3cb679957fa24f3249035aac3486d64cf02
Sven Rogge <sreglitzki@gmail.com>
2024-03-26 21:10:13 +0100

    Improve compilation scripts

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./connections/connections";
 export { getDb, migrationProvider, setupDb } from "./core/db/db";
+export * from "./pages/pages";
---
+++ packages/client/src/pages/pages.ts
@@ -0,0 +1,29 @@
+import { OmitProperties } from "ts-essentials";
+
+export type PageProps = {
+  collection?: string;
+  content?: object;
+  attributes?: Record<string, any>;
+  author?: object | string;
+};
+
+export type Page<C extends PageProps = PageProps> = {
+  slug: string;
+  title: string;
+  description?: string;
+  connection: number;
+  publishedAt: number;
+  createdAt: number;
+  updatedAt?: number;
+  changedAt: number;
+} & OmitProperties<
+  {
+    collection: C["collection"] extends string ? C["collection"] : never;
+    content: C["content"] extends object ? C["content"] : never;
+    attributes: C["attributes"] extends Record<string, any>
+      ? C["attributes"]
+      : never;
+    author: C["author"] extends object ? C["author"] : never;
+  },
+  never
+>;
