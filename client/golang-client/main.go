d6add2de55da0f964fac1bd17aab4282bad1b9b5
Sven Rogge <sreglitzki@gmail.com>
2024-10-13 12:15:44 +0200

    Start creating a go client

---
+++ client/golang-client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	u := url.URL{Scheme: "ws", Host: "localhost:3000", Path: "/"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	key := []byte{0x01}
+	key = append(key, []byte("123")...)
+	err = c.WriteMessage(websocket.BinaryMessage, key)
+	if err != nil {
+		log.Fatal("write:", err)
+	}
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+	}
+}
