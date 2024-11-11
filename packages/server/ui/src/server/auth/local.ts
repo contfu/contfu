e13235b00b9971ebbcc208f3160adcd8d8eb33e8
Sven Rogge <sreglitzki@gmail.com>
2024-11-11 06:17:44 +0100

    Use argon2 lib to be node compatible

--- packages/server/ui/src/server/auth/local.ts
+++ packages/server/ui/src/server/auth/local.ts
@@ -1,17 +1,24 @@
-import { db } from "@contfu/db";
+import { db, userTable } from "@contfu/db";
+import { hash, verify } from "argon2";
 import type { DisplayUser } from "./auth";
 import { createSession, generateSessionToken } from "./session";
 
 export async function login(email: string, password: string) {
-  const user = await db.query.user.findFirst({
+  let user = await db.query.user.findFirst({
     where: (user, { eq }) => eq(user.email, email),
   });
+  if (import.meta.env.DEV && !user) {
+    [user] = await db
+      .insert(userTable)
+      .values({
+        email: "test@test.com",
+        name: "Test User",
+        password: await hash("test"),
+      })
+      .returning();
+  }
 
-  if (
-    !user ||
-    !user.password ||
-    !(await Bun.password.verify(password, user.password))
-  )
+  if (!user || !user.password || !(await verify(user.password, password)))
     return null;
 
   const token = generateSessionToken();
