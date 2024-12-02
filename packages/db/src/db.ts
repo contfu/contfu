018cfd311576660778361314984aea7248937b3f
Sven Rogge <sreglitzki@gmail.com>
2024-12-02 06:46:40 +0100

    Add option for synchronization

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -4,12 +4,10 @@ import { migrate as drizzleMigrate } from "drizzle-orm/libsql/migrator";
 import { schema } from "./schema";
 
 const client = createClient({
+  syncUrl: process.env.DATABASE_SYNC_URL,
   url: process.env.DATABASE_URL ?? ":memory:",
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
-
-client.execute("PRAGMA foreign_keys = ON");
-client.execute("PRAGMA journal_mode = WAL");
 
 export const db = drizzle(client, { schema });
 
